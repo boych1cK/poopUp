8b3ccfcdc7164e8f8f671438466bb47b7d994dd7
Yaroslav <88162373+boych1cK@users.noreply.github.com>
2025-08-27 15:40:22 +0700

    Add files via upload

---
+++ poopup/poopUp.js
@@ -0,0 +1,37 @@
+class PoopUp {
+    constructor() {
+        this.active = false;
+        this.popUpContainer = null;
+        this.init();
+    }
+
+    init(){
+        if (!document.getElementById('message')) {
+            this.createPopUpContainer();
+        } else {
+            this.popUpContainer = document.getElementById('message');
+        }
+    }
+
+    createPopUpContainer() {
+        this.popUpContainer = document.createElement('div');
+        this.popUpContainer.id = 'message';
+        document.body.appendChild(this.popUpContainer);
+    }
+
+    showMessage(text, type) {
+        if(this.active === false){
+            this.active=true;
+            $(this.popUpContainer).removeClass('success error').addClass(type).text(text).fadeIn();
+            setTimeout(() => {
+                $(this.popUpContainer).fadeOut();
+                this.active = false
+            }, 3000);
+        }
+
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    window.poopUp = new PoopUp();
+});
